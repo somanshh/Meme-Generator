fed77cc9b851bd844b47f3f425f3cf919cccce9f
Somansh <rahulsoni0530@gmail.com>
2022-11-20 22:16:44 +0530

    Meme generator (Scrimba React project)

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./components/Navbar"
+import Form from "./components/Memes"
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react"
+import logo from "../images/troll--face.png"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src={logo} 
+                className="header--image"
+                alt=""
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
